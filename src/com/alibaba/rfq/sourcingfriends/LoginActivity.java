282d77087849796c32a71f17f43b72a53952b47b
lunatikoid <liang.yaol@alibaba-inc.com>
2014-01-06 23:16:01 +0800

    add anroidPN and it's successed!

--- src/com/alibaba/rfq/sourcingfriends/LoginActivity.java
+++ src/com/alibaba/rfq/sourcingfriends/LoginActivity.java
@@ -1,27 +1,18 @@
 package com.alibaba.rfq.sourcingfriends;
 
-import com.alibaba.rfq.sourcingfriends.service.BluetoothChatService;
-import com.alibaba.rfq.sourcingfriends.service.DeviceListActivity;
+import java.util.Properties;
+
+import org.androidpn.client.ServiceManager;
 import com.alibaba.rfq.sourcingfriends.tarbar.ManagerCenterActivity;
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.alibaba.rfq.sourcingfriends.service.nodejs.NodeJSConstant;
 
 /**
  * 这里，LoginActivity需要做的只是一个假连接，或者来说，真正的需要连接的时候，
@@ -31,17 +22,17 @@ import com.alibaba.rfq.sourcingfriends.service.nodejs.NodeJSConstant;
 
 public class LoginActivity extends Activity {
 
-	private Context context;
+	private Context gContext;
+
+	// server
+	private EditText serverIpEditText;
 
 	// login
 	private Button loginButton;
 	private EditText accountEditText;
 	private EditText passwdEditText;
-
-	// Bluetooth service is ok
-	private boolean isServiceOk = false;
-
-
+	private Properties props;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -51,64 +42,48 @@ public class LoginActivity extends Activity {
 		setContentView(R.layout.user_login);
 
 		//
-		context = this;
+		gContext = this;
 
 		loginButton = (Button) findViewById(R.id.loginButton);
 		accountEditText = (EditText) findViewById(R.id.accountEditText);
 		passwdEditText = (EditText) findViewById(R.id.passwdEditText);
-
-		isServiceOk = true;
-
-		loginButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				if (true == isServiceOk) {
-					// use accountEditText & passwdEditText to test the user is
-					// valid ???
-					{
-						Intent intent = new Intent();
-						intent.setClass(LoginActivity.this,
-								ManagerCenterActivity.class);
-						startActivity(intent);
-					}
-				} else {
-					Toast.makeText(
-							context,
-							"service is not ok,pls be sure linked to a device,try connect a device",
-							Toast.LENGTH_SHORT).show();
-				}
-			}
-		});
+		
+		// connect to the server
+		// connect to server
+        serverIpEditText = (EditText) findViewById(R.id.serverIpEditTextId );
+        
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+   
+            	props = new Properties();
+            	props.setProperty( "xmppHost", serverIpEditText.getText().toString() );
+            	props.setProperty( "apiKey", accountEditText.getText().toString());
+            	props.setProperty("xmppPort", "5222");
+            	
+            	class myThread extends Thread{
+            		public myThread(){
+            			Log.i("LoginActivity","create a Thread");
+            		}
+            		public void run(){
+            	// connect to server
+                ServiceManager serviceManager = new ServiceManager(gContext,props);
+                serviceManager.setNotificationIcon(R.drawable.notification);
+                serviceManager.startService();
+            	}
+            	};
+            	
+            	myThread aThread = new myThread();
+            	aThread.run();
+            	Log.i("LoginActivity","It's Here");
+                //Intent intent = new Intent();
+				//intent.setClass(LoginActivity.this,
+				//		ManagerCenterActivity.class);
+				//startActivity(intent);
+                
+            }
+        });
+		
+		// ended
 
 	}
-
-	// thread
-	Handler handler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			super.handleMessage(msg);
-			Bundle data = msg.getData();
-			String val = data.getString("error");
-			if (val != null) {
-				Toast.makeText(context, "err", Toast.LENGTH_SHORT).show();
-			} else if ((val = data.getString("jGet:value")) != null) {
-				Toast.makeText(context, val, Toast.LENGTH_SHORT).show();
-			} else if ((val = data.getString("jPost:value")) != null) {
-				Toast.makeText(context, val, Toast.LENGTH_SHORT).show();
-			} else if ((val = data.getString("aGet:value")) != null) {
-				Toast.makeText(context, val, Toast.LENGTH_SHORT).show();
-			} else if ((val = data.getString("aPost:value")) != null) {
-				Toast.makeText(context, val, Toast.LENGTH_SHORT).show();
-			} else
-				Toast.makeText(context, "err", Toast.LENGTH_SHORT).show();
-
-			// Toast.makeText(context, "javaGet+" + val,
-			// Toast.LENGTH_SHORT).show();
-
-			Log.i("login", "请求结果为-->" + val);
-		}
-	};
-
 }
--- src/com/alibaba/rfq/sourcingfriends/msgcenter/IngMessageActivity.java
+++ src/com/alibaba/rfq/sourcingfriends/msgcenter/IngMessageActivity.java
@@ -6,8 +6,6 @@ import java.util.List;
 import com.alibaba.rfq.sourcingfriends.R;
 import com.alibaba.rfq.sourcingfriends.db.DatabaseService;
 import com.alibaba.rfq.sourcingfriends.dto.UserProfileDTO;
-import com.alibaba.rfq.sourcingfriends.service.BluetoothChatService;
-import com.alibaba.rfq.sourcingfriends.service.DeviceListActivity;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
@@ -48,34 +46,11 @@ public class IngMessageActivity extends Activity {
 
 	private String userIdStr = "";
 
-	// Key names received from the BluetoothChatService Handler
-	public static final String DEVICE_NAME = "device_name";
-	public static final String TOAST = "toast";
-
-	// Name of the connected device
-	private String mConnectedDeviceName = null;
 	// Array adapter for the conversation thread
 	// private ArrayAdapter<String> mConversationArrayAdapter;
 	private IngMessageAdapter mConversationIngMsgAdapter;
-	// String buffer for outgoing messages
-	private StringBuffer mOutStringBuffer;
-	// Member object for the chat services
-	private BluetoothChatService mChatService = null;
 
-	// Message types sent from the BluetoothChatService Handler
-	public static final int MESSAGE_STATE_CHANGE = 1;
-	public static final int MESSAGE_READ = 2;
-	public static final int MESSAGE_WRITE = 3;
-	public static final int MESSAGE_DEVICE_NAME = 4;
-	public static final int MESSAGE_TOAST = 5;
-
-	// Intent request codes
-	private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
-	private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
-	private static final int REQUEST_ENABLE_BT = 3;
-
-	// Local Bluetooth adapter
-	private BluetoothAdapter mBluetoothAdapter = null;
+	
 	private DatabaseService service = null;
 	private UserProfileDTO she = null ;
 	private UserProfileDTO me = null ;
@@ -90,8 +65,7 @@ public class IngMessageActivity extends Activity {
 		// tem for add person to sqlite
 		Bitmap photo = BitmapFactory.decodeResource(this.getResources(),
 				R.drawable.yliang);
-		// Get local Bluetooth adapter
-		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		
 		UserProfileDTO user = new UserProfileDTO("yliang", "1111",
 				"alibaba-inc", photo);
 
@@ -104,16 +78,10 @@ public class IngMessageActivity extends Activity {
 		Log.i(TAG,""+she.getUserName()+" "+me.getId());
 		// end
 
-		// If the adapter is null, then Bluetooth is not supported
-		if (mBluetoothAdapter == null) {
-			Toast.makeText(this, "Bluetooth is not available",
-					Toast.LENGTH_LONG).show();
-			finish();
-			return;
-		}
-
 		Intent intent = getIntent();
 		userIdStr = intent.getStringExtra("UserId");
+		
+		MsgIng();
 	}
 
 	private List<String> getMsgData(int id) {
@@ -141,63 +109,28 @@ public class IngMessageActivity extends Activity {
 
 		// Initialize the compose field with a listener for the return key
 		mOutEditText = (EditText) findViewById(R.id.Text4SendEditText);
-		mOutEditText.setOnEditorActionListener(mWriteListener);
+		//mOutEditText.setOnEditorActionListener(mWriteListener);
 
 		// Initialize the send button with a listener that for click events
 		mSendButton = (Button) findViewById(R.id.button_send);
 		mSendButton.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				// Send a message using content of the edit text widget
-				TextView view = (TextView) findViewById(R.id.Text4SendEditText);
-				String message = view.getText().toString();
-				sendMessage(message);
+
+				//sendMessage;
 			}
 		});
 
-		// Initialize the BluetoothChatService to perform bluetooth connections
-		mChatService = new BluetoothChatService(this, mHandler);
-
-		// Initialize the buffer for outgoing messages
-		mOutStringBuffer = new StringBuffer("");
 	}
 
 	@Override
 	public void onStart() {
 		super.onStart();
-		if (D)
-			Log.e(TAG, "++ ON START ++");
-
-		// If BT is not on, request that it be enabled.
-		// setupChat() will then be called during onActivityResult
-		if (!mBluetoothAdapter.isEnabled()) {
-			Intent enableIntent = new Intent(
-					BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-			// Otherwise, setup the chat session
-		} else {
-			if (mChatService == null)
-				MsgIng();
-		}
 	}
 
 	@Override
 	public synchronized void onResume() {
 		super.onResume();
-		if (D)
-			Log.e(TAG, "+ ON RESUME +");
-
-		// Performing this check in onResume() covers the case in which BT was
-		// not enabled during onStart(), so we were paused to enable it...
-		// onResume() will be called when ACTION_REQUEST_ENABLE activity
-		// returns.
-		if (mChatService != null) {
-			// Only if the state is STATE_NONE, do we know that we haven't
-			// started already
-			if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
-				// Start the Bluetooth chat services
-				mChatService.start();
-			}
-		}
 	}
 
 	@Override
@@ -217,121 +150,24 @@ public class IngMessageActivity extends Activity {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		// Stop the Bluetooth chat services
-		if (mChatService != null)
-			mChatService.stop();
-		if (D)
-			Log.e(TAG, "--- ON DESTROY ---");
 	}
-
-	private void ensureDiscoverable() {
-		if (D)
-			Log.d(TAG, "ensure discoverable");
-		if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
-			Intent discoverableIntent = new Intent(
-					BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-			discoverableIntent.putExtra(
-					BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-			startActivity(discoverableIntent);
-		}
-	}
-
-	/**
-	 * Sends a message.
-	 * 
-	 * @param message
-	 *            A string of text to send.
-	 */
-	private void sendMessage(String message) {
-		// Check that we're actually connected before trying anything
-		if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
-			Toast.makeText(this, R.string.not_connected, Toast.LENGTH_SHORT)
-					.show();
-			return;
-		}
-
-		// Check that there's actually something to send
-		if (message.length() > 0) {
-			// Get the message bytes and tell the BluetoothChatService to write
-			byte[] send = message.getBytes();
-			mChatService.write(send);
-
-			// Reset out string buffer to zero and clear the edit text field
-			mOutStringBuffer.setLength(0);
-			mOutEditText.setText(mOutStringBuffer);
-		}
-	}
-
-	// The action listener for the EditText widget, to listen for the return key
-	private TextView.OnEditorActionListener mWriteListener = new TextView.OnEditorActionListener() {
-		public boolean onEditorAction(TextView view, int actionId,
-				KeyEvent event) {
-			// If the action is a key-up event on the return key, send the
-			// message
-			if (actionId == EditorInfo.IME_NULL
-					&& event.getAction() == KeyEvent.ACTION_UP) {
-				String message = view.getText().toString();
-				sendMessage(message);
-			}
-			if (D)
-				Log.i(TAG, "END onEditorAction");
-			return true;
-		}
-	};
-
-	// The Handler that gets information back from the BluetoothChatService
-	private final Handler mHandler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			switch (msg.what) {
-			case MESSAGE_STATE_CHANGE:
-				// if(D) Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
-				switch (msg.arg1) {
-				case BluetoothChatService.STATE_CONNECTED:
-					user2TalkTextView.setText(R.string.title_connected_to);
-					user2TalkTextView.append(mConnectedDeviceName);
-					// mConversationArrayAdapter.clear();
-					break;
-				case BluetoothChatService.STATE_CONNECTING:
-					user2TalkTextView.setText(R.string.title_connecting);
-					break;
-				case BluetoothChatService.STATE_LISTEN:
-				case BluetoothChatService.STATE_NONE:
-					user2TalkTextView.setText(R.string.title_not_connected);
-					break;
-				}
-				break;
-			case MESSAGE_WRITE:
-				byte[] writeBuf = (byte[]) msg.obj;
-				// construct a string from the buffer
-				String writeMessage = new String(writeBuf);
-				mConversationIngMsgAdapter.send("she|" + writeMessage);
-				break;
-			case MESSAGE_READ:
-				byte[] readBuf = (byte[]) msg.obj;
-				// construct a string from the valid bytes in the buffer
-				String readMessage = new String(readBuf, 0, msg.arg1);
-				mConversationIngMsgAdapter.receive("me|" + readMessage);
-				break;
-			case MESSAGE_DEVICE_NAME:
-				// save the connected device's name
-				mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
-				Toast.makeText(getApplicationContext(),
-						"Connected to " + mConnectedDeviceName,
-						Toast.LENGTH_SHORT).show();
-				break;
-			case MESSAGE_TOAST:
-				Toast.makeText(getApplicationContext(),
-						msg.getData().getString(TOAST), Toast.LENGTH_SHORT)
-						.show();
-				break;
-			}
-		}
-	};
-
-	/**
-	 * Menu settup select a way to connect server
-	 * */
+	
+//	// The action listener for the EditText widget, to listen for the return key
+//	private TextView.OnEditorActionListener mWriteListener = new TextView.OnEditorActionListener() {
+//		public boolean onEditorAction(TextView view, int actionId,
+//				KeyEvent event) {
+//			// If the action is a key-up event on the return key, send the
+//			// message
+//			if (actionId == EditorInfo.IME_NULL
+//					&& event.getAction() == KeyEvent.ACTION_UP) {
+//				String message = view.getText().toString();
+//				//sendMessage(message);
+//			}
+//			if (D)
+//				Log.i(TAG, "END onEditorAction");
+//			return true;
+//		}
+//	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -340,76 +176,4 @@ public class IngMessageActivity extends Activity {
 		return true;
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		return onItemSelected(this, item.getItemId());
-	}
-
-	public boolean onItemSelected(Context context, int id) {
-
-		Intent serverIntent = null;
-		switch (id) {
-		case R.id.secure_connect_scan:
-			// Launch the DeviceListActivity to see devices and do scan
-			serverIntent = new Intent(context, DeviceListActivity.class);
-			startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
-			return true;
-		case R.id.insecure_connect_scan:
-			// Launch the DeviceListActivity to see devices and do scan
-			serverIntent = new Intent(context, DeviceListActivity.class);
-			startActivityForResult(serverIntent,
-					REQUEST_CONNECT_DEVICE_INSECURE);
-			return true;
-		case R.id.discoverable:
-			// Ensure this device is discoverable by others
-			ensureDiscoverable();
-			return true;
-		}
-		return false;
-
-	}
-
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (D)
-			Log.d(TAG, "onActivityResult " + resultCode);
-		switch (requestCode) {
-		case REQUEST_CONNECT_DEVICE_SECURE:
-			// When DeviceListActivity returns with a device to connect
-			if (resultCode == Activity.RESULT_OK) {
-
-				connectDevice(data, true);
-			}
-			break;
-		case REQUEST_CONNECT_DEVICE_INSECURE:
-			// When DeviceListActivity returns with a device to connect
-			if (resultCode == Activity.RESULT_OK) {
-
-				connectDevice(data, false);
-			}
-			break;
-		case REQUEST_ENABLE_BT:
-			// When the request to enable Bluetooth returns
-			if (resultCode == Activity.RESULT_OK) {
-				// Bluetooth is now enabled, so set up a chat session
-				MsgIng();
-			} else {
-				// User did not enable Bluetooth or an error occured
-				Log.d(TAG, "BT not enabled");
-				Toast.makeText(this, R.string.bt_not_enabled_leaving,
-						Toast.LENGTH_SHORT).show();
-				finish();
-			}
-		}
-	}
-
-	private void connectDevice(Intent data, boolean secure) {
-		// Get the device MAC address
-		String address = data.getExtras().getString(
-				DeviceListActivity.EXTRA_DEVICE_ADDRESS);
-		// Get the BLuetoothDevice object
-		BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
-		// Attempt to connect to the device
-		mChatService.connect(device, secure); // 不在此时调用
-												// mChatService.connect方法，需要在IngMessageActivity中调用
-	}
 }
